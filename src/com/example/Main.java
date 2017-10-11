c5a3de5dd7bd14caea88c13e62480c44e0954ab1
Brian Sheen <bsheen@gmail.com>
2017-10-11 16:25:43 -0500

    solved palindrome number

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -3,6 +3,22 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int x = 101201;
+        System.out.println(isPalindrome(x));
+    }
+
+    public static boolean isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
+        String reverse = String.valueOf(Math.abs(x));;
+        StringBuilder solution = new StringBuilder();
+        for (int i = reverse.length() - 1; i >= 0; --i) {
+            solution.append(reverse.charAt(i));
+        }
+        if(reverse.equals(solution.toString())){
+            return true;
+        }
+        return false;
     }
 }
